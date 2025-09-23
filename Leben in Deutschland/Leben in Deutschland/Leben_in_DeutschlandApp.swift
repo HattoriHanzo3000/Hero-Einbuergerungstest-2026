409a061358cbc465425c999657cf7fb478ce188f
Ildar Gizatullin <gizatullin.ildar@gmail.com>
2025-09-23 11:12:42 +0200

    Initial commit - clean project setup

---
+++ Leben in Deutschland/Leben in Deutschland/Leben_in_DeutschlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leben_in_DeutschlandApp.swift
+//  Leben in Deutschland
+//
+//  Created by Ildar Gizatullin on 23.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Leben_in_DeutschlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
